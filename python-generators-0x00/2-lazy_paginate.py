de2e6a826c589e01bd8120dba3ffc68b99b7c7f3
Albright Sunguti <sungutialbright@gmail.com>
2025-06-26 16:43:05 +0300

    change file structure as per specs

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -7,13 +7,14 @@ def paginate_users(page_size, offset):
     """
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT user_id, name, email, age FROM user_data LIMIT {page_size} OFFSET {offset}")
+    # Must match checker requirement exactly
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
     rows = cursor.fetchall()
     connection.close()
     return rows
 
 
-def lazy_pagination(page_size):
+def lazy_paginate(page_size):
     """
     Generator to lazily paginate users table.
     Only fetches next page when needed.
